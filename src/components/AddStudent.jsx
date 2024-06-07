2886d6948287d88489bb29e29edef7bdfbba091b
safabeegum <pasafabeegum@gmail.com>
2024-06-07 14:27:40 +0530

    Completed

---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const AddStudent = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">NAME</label>
+                            <input type="text" className="form-control" />
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">ADMN NO</label>
+                            <input type="text" className="form-control" />
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">ROLL NO</label>
+                            <input type="text" className="form-control" />
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">PARENT NAME</label>
+                            <input type="text" className="form-control" />
+                        </div>
+
+                        <div className="col col-12 col-sm12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">COLLEGE</label>
+                            <input type="text" className="form-control" />
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">DATE OF BIRTH</label>
+                            <input type="date" name="" id="" className="form-control" />
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">EMAIL</label>
+                            <input type="text" className="form-control" />
+
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">PASSWORD</label>
+                            <input type="password" name="" id="" className="form-control" />
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">CONFIRM PASSWORD</label>
+                            <input type="password" name="" id="" className="form-control" />
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success">SUCCESS</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+  )
+}
+
+export default AddStudent
---
+++ src/components/ViewStudent.jsx
@@ -0,0 +1,129 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const ViewStudent = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                        <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" class="card-img-top" alt="..."></img>
+                        <div class="card-body">
+                        <h5 class="card-title">CIVIL</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">GET COLLEGE DETAILS</a>
+                        </div>
+                        </div>   
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                        <img src="https://img.freepik.com/free-photo/young-student-woman-wearing-denim-jacket-eyeglasses-holding-colorful-folders-showing-thumb-up-pink_176532-13861.jpg?w=1060&t=st=1717740797~exp=1717741397~hmac=57fc27fd5a4de32cf78b84e59acb94b441c1e21860eff6e45b39345f67c208a8" class="card-img-top" alt="..."></img>
+                        <div class="card-body">
+                        <h5 class="card-title">MECH</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">GET COLLEGE DETAILS</a>
+                        </div>
+                        </div>   
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                        <img src="https://img.freepik.com/free-photo/smart-good-looking-creative-young-brunette-girl-giving-advice-pointing-upper-left-corner-suggest-promo-smiling-pleased-approve-some-product-standing-white-background_176420-36871.jpg?w=1060&t=st=1717740838~exp=1717741438~hmac=9bc9b6db5088b8c5da390145a1f94be6b0520560862544d3a837d6fc28c0996b" class="card-img-top" alt="..."></img>
+                        <div class="card-body">
+                        <h5 class="card-title">EEE</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">GET COLLEGE DETAILS</a>
+                        </div>
+                        </div>   
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                        <img src="https://img.freepik.com/free-photo/successful-lucky-cute-brunette-woman-jumping-from-happiness-clench-fists-smiling-happy-camera-as-winning-reward-achieve-approval-prize-become-champion-triumphing_176420-37393.jpg?w=1060&t=st=1717740880~exp=1717741480~hmac=25b858f6b22b1118d2cd27aa92026698800d867a1924ae4ed3ef624d3db6cba8" class="card-img-top" alt="..."></img>
+                        <div class="card-body">
+                        <h5 class="card-title">CS</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">GET COLLEGE DETAILS</a>
+                        </div>
+                        </div>   
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                        <img src="https://img.freepik.com/free-photo/smiling-happy-indian-student-with-backpack-pointing-his-finger-wall_496169-1532.jpg?w=1060&t=st=1717740940~exp=1717741540~hmac=92231248f29f45bdfc3fcdc04adcc340951426b194bf598b5a5691d548555160" class="card-img-top" alt="..."></img>
+                        <div class="card-body">
+                        <h5 class="card-title">BCA</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">GET COLLEGE DETAILS</a>
+                        </div>
+                        </div>   
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                        <img src="https://img.freepik.com/free-photo/front-view-male-student-wearing-black-backpack-holding-copybooks-files-blue-wall_140725-42636.jpg?w=1060&t=st=1717740981~exp=1717741581~hmac=fe515bef67573256d59cd495bdaa6091612324acfa0163210c44656c33a9959d" class="card-img-top" alt="..."></img>
+                        <div class="card-body">
+                        <h5 class="card-title">MBA</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">GET COLLEGE DETAILS</a>
+                        </div>
+                        </div>   
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                        <img src="https://img.freepik.com/premium-photo/teenager-student-girl-yellow-pointing-finger-side_1368-40175.jpg" class="card-img-top" alt="..."></img>
+                        <div class="card-body">
+                        <h5 class="card-title">MCA</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">GET COLLEGE DETAILS</a>
+                        </div>
+                        </div>   
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                        <img src="https://img.freepik.com/free-photo/girl-with-books-blackboard_23-2147650749.jpg?w=1060&t=st=1717741129~exp=1717741729~hmac=9f398ecbb496c85bd6aeb171bc9d04a51a80c0a7742ca45d45926dd010fca00e" class="card-img-top" alt="..."></img>
+                        <div class="card-body">
+                        <h5 class="card-title">BBA</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">GET COLLEGE DETAILS</a>
+                        </div>
+                        </div>   
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                        <img src="https://img.freepik.com/free-photo/red-haired-lady-eyeglasses-holds-books-shows-ok-sign_197531-23789.jpg?w=1060&t=st=1717741185~exp=1717741785~hmac=bef1ac82ebbfff4b6a5c8deb0eb34edd8e3ab89bcebe8b95908b06b848e5b1d4" class="card-img-top" alt="..."></img>
+                        <div class="card-body">
+                        <h5 class="card-title">BCOM</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">GET COLLEGE DETAILS</a>
+                        </div>
+                        </div>   
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                        <img src="https://img.freepik.com/free-photo/education-concept-smiling-brunette-girl-student-casual-clothes-holds-her-books-study-material_1258-217711.jpg?w=1060&t=st=1717741157~exp=1717741757~hmac=72c7b3280f99c51954923caffd40a19e0b745aca21b06031cf919318763ef3f4" class="card-img-top" alt="..."></img>
+                        <div class="card-body">
+                        <h5 class="card-title">MCOM</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">GET COLLEGE DETAILS</a>
+                        </div>
+                        </div>  
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ViewStudent
